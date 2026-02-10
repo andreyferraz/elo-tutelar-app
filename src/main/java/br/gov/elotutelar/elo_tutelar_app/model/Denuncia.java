f6fa03e88768092c5f5019749ea7982ebca0d5b2
andreyferraz <andrey.developer@hotmail.com>
2026-02-09 22:35:18 -0300

    Add Denuncia entity and TipoViolencia enum; include Spring Data JPA and Lombok dependencies

---
+++ src/main/java/br/gov/elotutelar/elo_tutelar_app/model/Denuncia.java
@@ -0,0 +1,34 @@
+package br.gov.elotutelar.elo_tutelar_app.model;
+
+import java.util.UUID;
+
+import br.gov.elotutelar.elo_tutelar_app.unums.TipoViolencia;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "denuncia")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Denuncia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    String protocolo;
+    @Column(length = 5000)
+    String descricao;
+    TipoViolencia tipoViolencia;
+
+}
---
+++ src/main/java/br/gov/elotutelar/elo_tutelar_app/unums/TipoViolencia.java
@@ -0,0 +1,5 @@
+package br.gov.elotutelar.elo_tutelar_app.unums;
+
+public enum TipoViolencia {
+
+}
