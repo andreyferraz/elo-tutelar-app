1f3691e1f90c3c5dce144e8e129cca69a40fb7c3
andreyferraz <andrey.developer@hotmail.com>
2026-02-09 22:05:13 -0300

    Initialize Spring Boot application with Maven wrapper and necessary configurations

---
+++ src/main/java/br/gov/elotutelar/elo_tutelar_app/EloTutelarAppApplication.java
@@ -0,0 +1,13 @@
+package br.gov.elotutelar.elo_tutelar_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloTutelarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloTutelarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/elotutelar/elo_tutelar_app/EloTutelarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.elotutelar.elo_tutelar_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloTutelarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
