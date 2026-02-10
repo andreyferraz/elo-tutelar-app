2a0efd0f5acdb50609f9d47046cbab66dca2d890
andreyferraz <andrey.developer@hotmail.com>
2026-02-09 22:39:13 -0300

    Enhance Denuncia model with additional fields and create StatusDenuncia enum

--- src/main/java/br/gov/elotutelar/elo_tutelar_app/model/Denuncia.java
+++ src/main/java/br/gov/elotutelar/elo_tutelar_app/model/Denuncia.java
@@ -1,7 +1,9 @@
 package br.gov.elotutelar.elo_tutelar_app.model;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.gov.elotutelar.elo_tutelar_app.unums.StatusDenuncia;
 import br.gov.elotutelar.elo_tutelar_app.unums.TipoViolencia;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -25,10 +27,18 @@ public class Denuncia {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
     String protocolo;
     @Column(length = 5000)
     String descricao;
     TipoViolencia tipoViolencia;
+    LocalDateTime dataHoraRegistro;
+    StatusDenuncia status;
+
+    String endereco;
+    String numero;
+    String bairro;
+    String pontoReferencia;
+    String nomeVitima;
+    String provavelAutor;
 
 }
